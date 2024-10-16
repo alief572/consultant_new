88a4aeec593a48d23b3e5c20ed6a5da1173173e7
alief572 <146901652+alief572@users.noreply.github.com>
2024-10-16 16:35:36 +0700

    Perbaikan - Users
    Tambahkan inputan untuk employee id

--- application/modules/users/controllers/Users.php
+++ application/modules/users/controllers/Users.php
@@ -1,12 +1,13 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Users extends Front_Controller {
+class Users extends Front_Controller
+{
 
     public function __construct()
     {
-    	parent::__construct();
-    	$this->load->model(array('identitas_model'));
-    	$this->load->library('users/auth');
+        parent::__construct();
+        $this->load->model(array('identitas_model'));
+        $this->load->library('users/auth');
 
         $this->id_user  = $this->auth->user_id();
     }
@@ -18,35 +19,33 @@ class Users extends Front_Controller {
 
     public function login()
     {
-        if($this->auth->is_login())
-        {
+        if ($this->auth->is_login()) {
             history("Login");
             redirect('/');
         }
 
-    	//$identitas = $this->identitas_model->find(1); => ERROR variable nama_program not define krn ga ada fieldnya di tabel identitas
-        $identitas = $this->identitas_model->find_by(array('ididentitas'=>1));// By Muhaemin => Di Form Login
+        //$identitas = $this->identitas_model->find(1); => ERROR variable nama_program not define krn ga ada fieldnya di tabel identitas
+        $identitas = $this->identitas_model->find_by(array('ididentitas' => 1)); // By Muhaemin => Di Form Login
 
-    	if(isset($_POST['login']))
-    	{
-    		$username = $this->input->post('username');
-    		$password = $this->input->post('password');
+        if (isset($_POST['login'])) {
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
 
-    		$this->auth->login($username, $password);
-    	}
+            $this->auth->login($username, $password);
+        }
 
-    	$this->template->set('idt', $identitas);
+        $this->template->set('idt', $identitas);
         $this->template->set_theme('default');
         $this->template->set_layout('login');
         $this->template->title('Login');
-    	$this->template->render('login_animate');
+        $this->template->render('login_animate');
     }
 
     public function logout()
-    {   
-        if(!empty($this->id_user)){
+    {
+        if (!empty($this->id_user)) {
             history("Logout");
         }
-    	$this->auth->logout();
+        $this->auth->logout();
     }
 }
