11b2d2f6bf53914b74df364c332f96b3ee9bc466
alief572 <146901652+alief572@users.noreply.github.com>
2025-05-26 15:59:12 +0700

    Perbaikan Data - Pengajuan, Expense

---
+++ application/modules/perbaikan_data/models/Perbaikan_data_model.php
@@ -0,0 +1,43 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ * @author Harboens
+ * @copyright Copyright (c) 2020
+ *
+ * This is model class for table "Budget Rutin"
+ */
+
+class Perbaikan_data_model extends BF_Model
+{
+    public function no_sendigs($tipe, $no)
+    {
+        $no_doc = '';
+        $newcode = '';
+        $data = $this->db->get_where(DBSF . '.ms_generate', array('tipe' => $tipe))->row();
+        if ($data !== false) {
+            if (stripos($data->info, 'YEAR', 0) !== false) {
+                if ($data->info3 != date("Y")) {
+                    $years = date("Y");
+                    $number = 1;
+                    $newnumber = sprintf('%0' . $data->info4 . 'd', $number);
+                } else {
+                    $years = $data->info3;
+                    $number = ($data->info2 + 1);
+                    $newnumber = sprintf('%0' . $data->info4 . 'd', $number);
+                }
+                $newcode = str_ireplace('XXXX', $newnumber, $data->info);
+                $newcode = str_ireplace('YEAR', $years, $newcode);
+                $newdata = array('info2' => $number, 'info3' => $years);
+            } else {
+                $number = ($data->info2 + 1);
+                $newnumber = sprintf('%0' . $data->info4 . 'd', $number);
+                $newcode = str_ireplace('XXXX', $newnumber, $data->info);
+                $newdata = array('info2' => $number);
+            }
+            $this->db->update(DBSF . '.ms_generate', $newdata, array('tipe' => $tipe));
+
+            $no_doc = $newcode;
+        }
+
+        return $no_doc;
+    }
+}
