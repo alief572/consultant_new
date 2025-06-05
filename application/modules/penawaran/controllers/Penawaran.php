1d2baeab067dd66494d1f61e3972c9345d43a102
alief572 <146901652+alief572@users.noreply.github.com>
2025-06-05 14:22:20 +0700

    Perbaikan - Penawaran
    Penambahan Puspita sebagai Sales

--- application/modules/penawaran/controllers/Penawaran.php
+++ application/modules/penawaran/controllers/Penawaran.php
@@ -81,7 +81,7 @@ class Penawaran extends Admin_Controller
 
         $this->db->select('a.id, a.name as nm_karyawan');
         $this->db->from(DBHR . '.employees a');
-        $this->db->where_in('a.id', ['EMP0010', 'EMP0029', 'EMP0031', 'EMP0170', 'EMP0246', 'EMP0035', 'EMP0001']);
+        $this->db->where_in('a.id', ['EMP0010', 'EMP0029', 'EMP0031', 'EMP0170', 'EMP0246', 'EMP0035', 'EMP0001', 'EMP0257']);
         $get_marketing = $this->db->get()->result();
 
         $this->db->select('a.id, a.name as nm_karyawan');
@@ -190,7 +190,7 @@ class Penawaran extends Admin_Controller
 
         $this->db->select('a.id, a.name as nm_karyawan');
         $this->db->from(DBHR . '.employees a');
-        $this->db->where_in('a.id', ['EMP0010', 'EMP0029', 'EMP0031', 'EMP0170', 'EMP0246', 'EMP0035', 'EMP0001']);
+        $this->db->where_in('a.id', ['EMP0010', 'EMP0029', 'EMP0031', 'EMP0170', 'EMP0246', 'EMP0035', 'EMP0001', 'EMP0257']);
         $get_marketing = $this->db->get()->result();
 
         $this->db->select('a.id, a.name as nm_karyawan');
@@ -477,7 +477,7 @@ class Penawaran extends Admin_Controller
 
         $this->db->select('a.id, a.name as nm_karyawan');
         $this->db->from(DBHR . '.employees a');
-        $this->db->where_in('a.id', ['EMP0010', 'EMP0029', 'EMP0031', 'EMP0170', 'EMP0246', 'EMP0035', 'EMP0001']);
+        $this->db->where_in('a.id', ['EMP0010', 'EMP0029', 'EMP0031', 'EMP0170', 'EMP0246', 'EMP0035', 'EMP0001', 'EMP0257']);
         $get_marketing = $this->db->get()->result();
 
         $this->db->select('a.id, a.name as nm_karyawan');
