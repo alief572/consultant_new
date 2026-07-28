1a442ea25e811758c7292bd8af608c6de4c268f6
alief572 <146901652+alief572@users.noreply.github.com>
2026-07-28 13:22:09 +0700

    Fix: Expense Report Project
    Format Penomoran mengikuti dengan format penomoran Expense Report di program sendigs

--- application/modules/expense_report_project/models/Expense_report_project_model.php
+++ application/modules/expense_report_project/models/Expense_report_project_model.php
@@ -24,18 +24,39 @@ class Expense_report_project_model extends BF_Model
         $this->sendigs = $this->load->database('sendigs_finance', true);
     }
 
-    function generate_id_expense_report_header()
+    function generate_id_expense_report_header($tipe_data)
     {
-        $Ym             = date('ym');
-        $srcMtr            = "SELECT MAX(id) as maxP FROM kons_tr_expense_report_project_header WHERE id LIKE '%/EXP/H/" . date('Y') . "%' ";
-        $resultMtr        = $this->db->query($srcMtr)->result_array();
-        $angkaUrut2        = $resultMtr[0]['maxP'];
-        $urutan2        = (int)substr($angkaUrut2, 0, 4);
-        $urutan2++;
-        $urut2            = sprintf('%04s', $urutan2);
-        $kode_trans        = $urut2 . '/EXP/H/' . date('Y');
+        $no_doc = '';
+        $newcode = '';
+        $query_data = 'SELECT * FROM ms_generate WHERE tipe = "' . $tipe_data . '";';
+        $data = $this->sendigs->query($query_data)->row();
+        if ($data !== false) {
+            if (stripos($data->info, 'YEAR', 0) !== false) {
+                if ($data->info3 != date("Y")) {
+                    $years = date("Y");
+                    $number = 1;
+                    $newnumber = sprintf('%0' . $data->info4 . 'd', $number);
+                } else {
+                    $years = $data->info3;
+                    $number = ($data->info2 + 1);
+                    $newnumber = sprintf('%0' . $data->info4 . 'd', $number);
+                }
+                $newcode = str_ireplace('XXXX', $newnumber, $data->info);
+                $newcode = str_ireplace('YEAR', $years, $newcode);
+                $newdata = array('info2' => $number, 'info3' => $years);
+            } else {
+                $number = ($data->info2 + 1);
+                $newnumber = sprintf('%0' . $data->info4 . 'd', $number);
+                $newcode = str_ireplace('XXXX', $newnumber, $data->info);
+                $newdata = array('info2' => $number);
+            }
 
-        return $kode_trans;
+            $this->sendigs->update('ms_generate', $newdata, array('tipe' => $tipe_data));
+
+            $no_doc = $newcode;
+        }
+
+        return $no_doc;
     }
 
     public function list_jurnal_pph21($id_header)
