1c14263a3cf9d56d5358430f1c573a4277cf6bbe
alief572 <146901652+alief572@users.noreply.github.com>
2026-08-11 13:59:53 +0700

    Fix: View Expense Report Project

--- application/modules/expense_report_project/controllers/Expense_report_project.php
+++ application/modules/expense_report_project/controllers/Expense_report_project.php
@@ -1470,6 +1470,13 @@ class Expense_report_project extends Admin_Controller
 
         $get_kasbon_header = $this->db->get_where('kons_tr_kasbon_project_header a', ['a.id' => $id_header])->row();
 
+        $this->db->select('a.*, b.nm_sales, b.waktu_from, b.waktu_to, c.nm_paket');
+        $this->db->from('kons_tr_spk_budgeting a');
+        $this->db->join('kons_tr_spk_penawaran b', 'b.id_spk_penawaran = a.id_spk_penawaran', 'left');
+        $this->db->join('kons_master_konsultasi_header c', 'c.id_konsultasi_h = a.id_project', 'left');
+        $this->db->where('a.id_spk_budgeting', $get_kasbon_header->id_spk_budgeting);
+        $get_spk_budgeting = $this->db->get()->row();
+
         $datalist_item = [];
         $datalist_item_expense = [];
 
@@ -2004,7 +2011,8 @@ class Expense_report_project extends Admin_Controller
             'id_penawaran' => $get_kasbon_header->id_penawaran,
             'tipe' => $get_kasbon_header->tipe,
             'list_bank' => $get_bank_acc,
-            'list_jurnal_pph21' => $list_jurnal_pph21
+            'list_jurnal_pph21' => $list_jurnal_pph21,
+            'list_budgeting' => $get_spk_budgeting
         ];
 
         $this->template->set($data);
