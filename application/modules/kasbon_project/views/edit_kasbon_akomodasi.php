fb98c7667df8107c9db049cc5fa7d100578063c2
alief572 <146901652+alief572@users.noreply.github.com>
2026-08-31 09:49:24 +0700

    Fix: Pengajuan
    Inputan Deskripsi dibuat mandatory

--- application/modules/kasbon_project/views/edit_kasbon_akomodasi.php
+++ application/modules/kasbon_project/views/edit_kasbon_akomodasi.php
@@ -116,9 +116,9 @@ $ENABLE_DELETE  = has_permission('Kasbon_Project.Delete');
                     <td class="pd-5 valign-top" width="400">
                         <input type="date" name="tgl" id="" class="form-control form-control-sm" value="<?= date('Y-m-d', strtotime($header->tgl)) ?>" readonly>
                     </td>
-                    <th class="pd-5 valign-top" width="150">Description</th>
+                    <th class="pd-5 valign-top" width="150">Deskripsi / Keterangan <span class="text-danger">*</span></th>
                     <td class="pd-5 valign-top" width="400">
-                        <textarea name="deskripsi" id="" class="form-control form-control-sm"><?= $header->deskripsi ?></textarea>
+                        <textarea name="deskripsi" id="" class="form-control form-control-sm" required placeholder="Deskripsi / Keterangan"><?= $header->deskripsi ?></textarea>
                     </td>
                 </tr>
 
@@ -618,6 +618,16 @@ $ENABLE_DELETE  = has_permission('Kasbon_Project.Delete');
 
     $(document).on('submit', '#frm-data', function(e) {
         e.preventDefault();
+
+        var deskripsi = $('textarea[name="deskripsi"]').val().trim();
+        if (deskripsi == '') {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Warning !',
+                text: 'Deskripsi / Keterangan wajib diisi !'
+            });
+            return false;
+        }
 
         var no = "<?= $no ?>";
         var valid = 1;
